80f20ab27000140a6fc48c40f2af14559fe444ad
Guest User <student@epicodus.com>
2015-04-22 10:36:27 -0700

    Everything complete, sinatra and integration testing.

--- spec/title_case_integration_spec.rb
+++ spec/title_case_integration_spec.rb
@@ -1,6 +1,6 @@
 require('capybara/rspec')
-require('/app')
-Capabara.app = Sinatra::Application
+require('./app')
+Capybara.app = Sinatra::Application
 
 describe('the title case path', {:type => :feature}) do
   it('processes the user entry and returns it title cased') do
